59343e6d210cb8b6fdedc60317e1e245fac097ea
bhuvi3 <msbhuvabhuvi@gail.com>
2013-05-14 01:42:31 +0530

    initial commit

---
+++ linear search.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+  int arr[20],k;
+  int n,i,flag=0;
+  printf("enter the number of elements of the array\n");
+  scanf("%d",&n);
+  printf("enter the elements of the array\n");
+  for(i=0;i<n;i++)
+  {
+      scanf("%d",&arr[i]);
+  }
+  printf("enter the key element you want to search for\n");
+  scanf("%d",&k);
+  for(i=0;i<n;i++)
+  {
+      if(arr[i]==k)
+      {
+          printf("key found at pos %d\n",i+1);flag =1;break;
+      }
+  }
+  if(flag ==0)
+  {
+      printf("element not found\n");
+  }
+}
