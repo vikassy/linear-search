7a61488ca1efe32c0aba7bd98c57e6a15ed4bc38
vikas <vikasyaligar.it@gmail.com>
2013-05-14 23:24:42 +0530

    Removed file.txt and documented linear_search

--- linear search.c
+++ linear search.c
@@ -1,29 +1,41 @@
-#include<stdio.h>
-//main funstion
+//Code to do linear search
+//Input: Array
+//Output: Element found or not
+//By: Bhuvan
+//Date: <write the date here!!>
+
+#include <stdio.h>
+#include <stdlib.h> //for using malloc
+//main function
 void main()
 {
   int arr[20],k;
-  int n,i,flag=0;
+  int n,i;
+  //for dynamic allocation of memory
+  //int *array;
   //input
   printf("enter the number of elements of the array\n");
   scanf("%d",&n);
+  //or maybe you can make it dynamic
+  //array = (int *)malloc(sizeof(int));
   printf("enter the elements of the array\n");
   for(i=0;i<n;i++)
   {
-      scanf("%d",&arr[i]);
+    scanf("%d",&arr[i]);
   }
+  //for(i=0;i<n;i++)
+  //{
+  //  scanf("%d",&array[i]);
+  //}
   printf("enter the key element you want to search for\n");
   scanf("%d",&k);
   //linear search
   for(i=0;i<n;i++)
-  {
       if(arr[i]==k)
       {
-          printf("key found at pos %d\n",i+1);flag =1;break;
+          printf("key found at pos %d\n",i+1);
+          break;
       }
-  }
-  if(flag ==0)
-  {
+  if(i == n)
       printf("element not found\n");
-  }
 }
